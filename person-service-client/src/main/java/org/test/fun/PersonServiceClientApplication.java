bcbcf642b814aa86129ec211d998e38707cb46f8
i85 <i85@spk.no>
2018-03-22 11:47:36 +0100

    Renaming main applications, adding client application and updating dependencies

---
+++ person-service-client/src/main/java/org/test/fun/PersonServiceClientApplication.java
@@ -0,0 +1,72 @@
+package org.test.fun;
+
+import java.io.Console;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.converter.json.AbstractJsonHttpMessageConverter;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.test.fun.model.Person;
+
+@SpringBootApplication
+public class PersonServiceClientApplication implements CommandLineRunner {
+
+    public static void main(String argv[]) {
+        SpringApplication.run(PersonServiceClientApplication.class, argv);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public void run(String... args) {
+        Console console = System.console();
+        if (console == null && args.length == 0) {
+            throw new IllegalStateException("No attached console found and no name argument given");
+        }
+
+        String serviceUrl = System.getenv("PERSON_SERVICE_URL");
+        if (serviceUrl == null) {
+            throw new IllegalStateException("No person-service URL set");
+        }
+
+        if (console != null) {
+            String name = console.readLine("Insert person name:");
+            while (name != null) {
+                Person p = createPerson(name);
+                URI personUrl = restTemplate.postForLocation(serviceUrl, new HttpEntity<>(p));
+                System.out.println("Created person: " + personUrl);
+                name = console.readLine("Insert person name:");
+            }
+        }
+
+        for (String arg : args) {
+            Person p = createPerson(arg);
+            URI personUrl = restTemplate.postForLocation(serviceUrl, p);
+            System.out.println("Created person: " + personUrl);
+        }
+    }
+
+    private Person createPerson(String name) {
+        Person p = new Person();
+        p.setFirstName(name);
+        p.setLastName("Generated");
+        return p;
+    }
+
+    @Bean
+    public static RestTemplate produceRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+}
--- person-service/src/main/java/org/test/fun/PersonServiceApplication.java
+++ person-service/src/main/java/org/test/fun/PersonServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"org.test.fun.service", "org.test.fun.repo"})
-public class TestApplication {
+public class PersonServiceApplication {
     public static void main(String argv[]) {
-        SpringApplication.run(TestApplication.class, argv);
+        SpringApplication.run(PersonServiceApplication.class, argv);
     }
 }
